a243bf9f1d6816b49ecf5bd06bdfbcaafcb5d6c4
Johnny Zavala <jzavalafgz@gmail.com>
2017-11-14 03:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver()
+{
+  const array = drivers.push('Ralph');
+}
+function destructivelyPrependDriver()
+{
+  const array = drivers.unshift('Bob');
+}
+function destructivelyRemoveLastDriver()
+{
+  
+}
